f3765ddd1f887f965dbaeadafba74fde477f4d75
Matthew Ameh <matthewameh007@gmail.com>
2024-07-24 16:44:41 +0100

    first commit

--- src/authService.ts
+++ src/authService.ts
@@ -1,13 +1,16 @@
-
-import {auth} from '@/firebaseConfig'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from '@/firebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 export const signUp = async (email: string, password: string) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     return userCredential.user;
   } catch (error) {
-    throw new Error(error.message);
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error('An unknown error occurred during sign up.');
+    }
   }
 };
 
@@ -16,7 +19,11 @@ export const signIn = async (email: string, password: string) => {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
     return userCredential.user;
   } catch (error) {
-    throw new Error(error.message);
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error('An unknown error occurred during sign in.');
+    }
   }
 };
 
@@ -24,6 +31,10 @@ export const signOutUser = async () => {
   try {
     await signOut(auth);
   } catch (error) {
-    throw new Error(error.message);
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error('An unknown error occurred during sign out.');
+    }
   }
 };
